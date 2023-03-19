59900a41dd0f64ce2966bee1b2697bac5f3e9a44
cleghorn-riddingsIt <cleghorn-riddingsIt@users.noreply.github.com>
2023-03-19 18:08:41 +0000

    coe cleanup

--- Invoices/main.py
+++ Invoices/main.py
@@ -29,34 +29,26 @@ class Table:
         self.caption = caption
         self.rows = rows
 
-def checkInt(int_string:str)->float:
-    #takes a string parameter and returns the integer convertion or -1 if the string isn't value
-    cleaned_string=int_string.replace(",",".").replace("%","")# remove any comma's or % and replace with decimal points
-    result_float=-1.0
+def convert_to_float(value: str) -> float:
     try:
-        result_float=float(cleaned_string)     
+        cleaned_string = value.replace(",", ".").replace("%", "")
+        return float(cleaned_string)
     except ValueError:
-        print(f"{cleaned_string} is not a valid float")
-    return result_float
+        raise ValueError(f"{value} is not a valid float")
 
 
-def extractCheckItems(row:list):
-#expect the row object to have the following structure
-#['026 RolesImplementation', 'RolesImplementation     Kumar, Ram 202306', '100491434 Gassco Forretningssupport', '29.21', 'HUR', '1409,00', '41156,89', '25%', '51446,11']
-    check_items_row={}
-    consultant_details=str(row[1]).split()
-    check_items_row['summary']=consultant_details[0]
-    check_items_row['hours']=checkInt(row[3])
-    check_items_row['rate']=checkInt(row[5])
-    check_items_row['totalnet']=checkInt(row[6])
-    check_items_row['taxrate']=checkInt(row[7])
-    check_items_row['totalgross']=checkInt(row[7])
-    
-    
-        
-    #column 2 contains bothe the consultant name and the week/year so this needs to be extracted
-   
-    print(row)
+def extract_check_items(row: list) -> dict:
+    if len(row) < 8:
+        raise ValueError("Row must have at least 8 elements")
+
+    check_items_row = {}
+    consultant_details = str(row[1]).split()
+    check_items_row['summary'] = consultant_details[0]
+    check_items_row['hours'] = convert_to_float(row[3])
+    check_items_row['rate'] = convert_to_float(row[5])
+    check_items_row['total_net'] = convert_to_float(row[6])
+    check_items_row['tax_rate'] = convert_to_float(row[7])
+    check_items_row['total_gross'] = convert_to_float(row[8])
     return check_items_row
     
     
@@ -65,6 +57,7 @@ def extractCheckItems(row:list):
 def extract_invoice(invoice:Table,checkitems:Table)->Dict:
     invoice_dict={}
     checkitems_dict={}
+    linked_workitem_list=[]
     #extract the invoice details
     for i,row in enumerate(invoice.rows):
         invoice_dict[row[0]]=row[2]
@@ -76,20 +69,17 @@ def extract_invoice(invoice:Table,checkitems:Table)->Dict:
     for i in range(len(checkitems.rows)-1):    #ignore the last row as this is just a summation of all the previous rows
         checkitem_list=checkitems.rows[i]
         list_length=len(checkitem_list)
-        linked_work_item=""
         if list_length>1:
-            checkitems_dict=extractCheckItems(checkitem_list)
+            checkitems_dict=extract_check_items(checkitem_list)
         elif list_length==1:
-            linked_work_item=checkitem_list[0]
-            saved_index=i   #store where you where when the linked_work_item wa last applied
-            #at this point we need to iterate through the extractCheckItems that exist and add the linked_work_item field
+            linked_workitem_list.extend([i,checkitem_list[0]])
         invoice_dict['lineitems'+str(i)]=checkitems_dict
-    return invoice_dict
+    return (invoice_dict,linked_workitem_list)
 
 invoice_index=7     #any invoice information will be at index 7 in the table_objects 
 lineitems_index=9   #any lines items information will be at index 9 in the table_objects 
 invoice={}
-check_items={}
+linked_items={}
 
 
 table_objects = []
@@ -108,6 +98,6 @@ for table in tables:
     table_objects.append(table_object)
  
 #extract line items
-invoice=extract_invoice(table_objects[invoice_index],table_objects[lineitems_index])
+invoice,linked_items=extract_invoice(table_objects[invoice_index],table_objects[lineitems_index])
 
 print(table_objects)
